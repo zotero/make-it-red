085bdc2c4ff6cc575db9d63d589723f4ffc06686
Dan Stillman <dstillman@zotero.org>
2022-12-05 02:40:59 -0700

    Remove unnecessary waitForZotero() code in 1.1
    1.1 is only used in bootstrap mode in Zotero 7, which always makes the
    Zotero object available, so there's no need to look for it.

--- src-1.1/bootstrap.js
+++ src-1.1/bootstrap.js
@@ -11,47 +11,9 @@ function log(msg) {
 	Zotero.debug("Make It Red: " + msg);
 }
 
-// In Zotero 6, bootstrap methods are called before Zotero is initialized, and using include.js
-// to get the Zotero XPCOM service would risk breaking Zotero startup. Instead, wait for the main
-// Zotero window to open and get the Zotero object from there.
-//
 // In Zotero 7, bootstrap methods are not called until Zotero is initialized, and the 'Zotero' is
 // automatically made available.
 async function waitForZotero() {
-	if (typeof Zotero != 'undefined') {
-		await Zotero.initializationPromise;
-	}
-	
-	var { Services } = ChromeUtils.import("resource://gre/modules/Services.jsm");
-	var windows = Services.wm.getEnumerator('navigator:browser');
-	var found = false;
-	while (windows.hasMoreElements()) {
-		let win = windows.getNext();
-		if (win.Zotero) {
-			found = true;
-			break;
-		}
-	}
-	if (!found) {
-		await new Promise((resolve) => {
-			var listener = {
-				onOpenWindow: function (aWindow) {
-					// Wait for the window to finish loading
-					let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor)
-						.getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
-					domWindow.addEventListener("load", function () {
-						domWindow.removeEventListener("load", arguments.callee, false);
-						if (domWindow.Zotero) {
-							Services.wm.removeListener(listener);
-							Zotero = domWindow.Zotero;
-							resolve();
-						}
-					}, false);
-				}
-			};
-			Services.wm.addListener(listener);
-		});
-	}
 	await Zotero.initializationPromise;
 }
 
